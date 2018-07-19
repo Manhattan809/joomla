66f0d50e2f76f0cdfd5a80ed39f39a90ea8649e3
unknown <isma316005@gmail.com>
2018-07-19 15:37:59 -0400

    "finished project"

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-07-06 13:20:22 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-07-06T13:20:22+00:00	INFO ::1	joomlafailure	Empty password not allowed.
